a9d1cf019a5a2b1af32b5432a4b97fde163f8db8
Thembelihle Notyhawe <emmanuelnotyhawe@gmail.com>
2025-11-20 18:21:07 +0200

    went back to previous version

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,9 +1,7 @@
-import logo from './images/logo.svg'
-
 const HeaderSection = () => {
     return <header className="flex items-center justify-between pl-8 pr-8 pt-12 md:pl-16 md:pt-16 md:pr-16 relative">
         <div>
-            <img className="w-28" src={logo} alt="logo-image" />
+            <img className="w-28" src="../public/images/logo.svg" alt="logo-image" />
         </div>
         
         <nav>
